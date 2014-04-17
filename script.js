239456c7ddcd79fdcef59369930d45a66c82dd41
cmsla <tlsckdan@gmail.com>
2014-04-18 00:07:54 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  $('.A').click(function(){
+    $(this).toggleClass('backA');
+  });
+
+  $('.B').click(function(){
+    $(this).toggleClass('A1');
+  });
+  
+$('.C').click(function(){
+    $(this).toggleClass('C1');
+  }); 
+  
+$('.D').click(function(){
+    $(this).toggleClass('D1');
+  });  
+  
+$('.E').click(function(){
+    $(this).toggleClass('F1');
+  });
+  
+$('.F').click(function(){
+    $(this).toggleClass('B1');
+  });
+  
+$('.G').click(function(){
+    $(this).toggleClass('A1');
+  });
+  
+$('.H').click(function(){
+    $(this).toggleClass('C1');
+  });
+  
+$('.I').click(function(){
+    $(this).toggleClass('E1');
+  });  
+  
+$('.J').click(function(){
+    $(this).toggleClass('F1');
+  });  
+  
+$('.K').click(function(){
+    $(this).toggleClass('E1');
+  });  
+  
+$('.L').click(function(){
+    $(this).toggleClass('B1');
+  });    
+});
